d5d493276da865449fd10ccade09a951f227fe12
ai-kys <aykyshar@gmail.com>
2022-04-01 15:51:08 +0800

    default controller with templates

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @throws \Exception
+     */
+    public function homepage(EntityManagerInterface $em, Request $request): Response
+    {
+        $cr = $em->getRepository('App:Category');
+        $categories = $cr->findAll();
+        
+        return $this->render('default/index.html.twig', [
+            'categories' => $categories,
+            
+        ]);
+        
+    }
+
+    public function category(int $id, EntityManagerInterface $em) {
+        $category = $em->getRepository('App:Category')->find($id);
+        return $this->render('default/category.html.twig', [
+            'category' => $category,
+        ]);
+    }
+}
